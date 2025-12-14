a3614d3284a551ece7fb94045e61d9821c50764a
nonoro <kgu1022@naver.com>
2025-12-14 16:26:04 +0900

    feat: Member 도메인 모델 확장
    - Member 생성 정적 팩토리 메서드 추가
    - 비밀번호 검증 로직 추가
    - 비밀번호 변경 로직 추가
    - 닉네임 변경 로직 추가

---
+++ src/main/java/nonorospring/domain/PasswordEncoder.java
@@ -0,0 +1,6 @@
+package nonorospring.domain;
+
+public interface PasswordEncoder {
+    String encode(String password);
+    boolean matches(String password, String encodedPassword);
+}
--- src/test/java/nonorospring/domain/MemberTest.java
+++ src/test/java/nonorospring/domain/MemberTest.java
@@ -1,5 +1,6 @@
 package nonorospring.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -7,22 +8,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 class MemberTest {
+    Member member;
+    PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setup() {
+        this.passwordEncoder = new PasswordEncoder() {
+            @Override
+            public String encode(String password) {
+                return password.toUpperCase();
+            }
+
+            @Override
+            public boolean matches(String password, String encodedPassword) {
+                return encode(password).equals(encodedPassword);
+            }
+        };
+        member = Member.create("nonoro@splearn.app", "nonoro", "secret", passwordEncoder);
+    }
+
     @Test
     void createMember() {
-        var member = new Member("nonoro@splearn.app", "nonoro", "secret");
-
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
     }
 
     @Test
-    void constructorNullCheck() {
-        assertThatThrownBy(() -> new Member(null, "nonoro", "secret"))
-                .isInstanceOf(NullPointerException.class);
-    }
-
-    @Test
     void activate() {
-        var member = new Member("nonoro", "nonoro", "secret");
         member.activate();
 
         assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
@@ -30,7 +41,6 @@ class MemberTest {
 
     @Test
     void activateFail() {
-        var member = new Member("nonoro", "nonoro", "secret");
         member.activate();
 
         assertThatThrownBy(() -> {
@@ -40,7 +50,6 @@ class MemberTest {
 
     @Test
     void deactivate() {
-        var member = new Member("nonoro", "nonoro", "secret");
         member.activate();
 
         member.deactivated();
@@ -50,8 +59,6 @@ class MemberTest {
 
     @Test
     void deactivateFail() {
-        var member = new Member("nonoro", "nonoro", "secret");
-
         assertThatThrownBy(() -> member.deactivated()).isInstanceOf(IllegalStateException.class);
 
         member.activate();
@@ -60,4 +67,25 @@ class MemberTest {
         assertThatThrownBy(() -> member.deactivated()).isInstanceOf(IllegalStateException.class);
     }
 
+    @Test
+    void verifyPassword() {
+        assertThat(member.verifyPassword("secret", passwordEncoder)).isTrue();
+        assertThat(member.verifyPassword("hello", passwordEncoder)).isFalse();
+    }
+
+    @Test
+    void changeNickname() {
+        assertThat(member.getNickname()).isEqualTo("nonoro");
+
+        member.changeNickname("jungsun");
+
+        assertThat(member.getNickname()).isEqualTo("jungsun");
+    }
+
+    @Test
+    void changePassword() {
+        member.changePassword("verysecret", passwordEncoder);
+
+        assertThat(member.verifyPassword("verysecret", passwordEncoder)).isTrue();
+    }
 }
